cd230e064c49e3670b47291d5d098e958124dadc
jiaxu <jiaxu@papayamobile.cn>
2018-11-08 15:58:36 +0800

    fix

--- my_blog/my_blog/settings.py
+++ my_blog/my_blog/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '7duk9m3q-yltay^^wymgk6q__34t_d#7@etcj6^y9t6q1tgn%g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = '*'
 
